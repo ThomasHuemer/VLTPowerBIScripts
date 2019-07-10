4b57e960be13e85d833db2b7939ec758b6964638
Thomas Huemer <40458149+ThomasHuemer@users.noreply.github.com>
2019-07-10 17:52:51 +0200

    New

---
+++ VLTPowerBIScripts/CREATE_VLT_DB_POWERBI_NVCOMPLAINTTABLE.sql
@@ -0,0 +1,38 @@
+USE [VLT_AX500105_P]
+GO
+
+/****** Object:  View [dbo].[VLT_DB_POWERBI_SALESLINE]    Script Date: 23.07.2018 10:30:07 ******/
+IF EXISTS (
+		SELECT 1
+		FROM SYS.VIEWS
+		WHERE NAME = 'VLT_DB_POWERBI_NVCOMPLAINTTABLE'
+			AND TYPE = 'V'
+	)
+	DROP VIEW [dbo].[VLT_DB_POWERBI_NVCOMPLAINTTABLE]
+GO
+
+/****** Object:  View [dbo].[VLT_DB_POWERBI_NvCOMPLAINTTABLE]    Script Date: 23.07.2018 10:30:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE VIEW [dbo].[VLT_DB_POWERBI_NVCOMPLAINTTABLE]
+AS
+	SELECT ComplaintTable.COMPLAINTID,
+	ComplaintTable.RESPONSIBLEUSERID AS 'Verantwortlicher',
+	ComplaintTable.SALESID,
+	ComplaintTable.SALESGROUP,
+	ComplaintTable.PRODUCTNAME,
+	ComplaintTable.APPROVED,
+	ComplaintTable.APPROVEDBY,
+	ComplaintTable.APPROVEDDATE,
+	ComplaintTable.COMPLETE,
+	ComplaintTable.COMPLAINTID	+ ComplaintTable.DATAAREAID AS PKComplaintID,
+	ComplaintTable.SalesID	+ ComplaintTable.DATAAREAID AS FKSalesID
+	FROM NVCOMPLAINTTABLE AS ComplaintTable
+GO
+
