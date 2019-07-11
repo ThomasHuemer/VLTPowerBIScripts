149e37a7813096436efb9cd037bdf26886329e0e
Thomas Huemer <40458149+ThomasHuemer@users.noreply.github.com>
2019-07-11 13:44:38 +0200

    new

--- VLTPowerBIScripts/CREATE_VLT_DB_POWERBI_NVCOMPLAINTTABLE.sql
+++ VLTPowerBIScripts/CREATE_VLT_DB_POWERBI_NVCOMPLAINTTABLE.sql
@@ -4,7 +4,7 @@ GO
 /****** Object:  View [dbo].[VLT_DB_POWERBI_SALESLINE]    Script Date: 23.07.2018 10:30:07 ******/
 IF EXISTS (
 		SELECT 1
-		FROM SYS.VIEWS
+		FROM sys.VIEWS
 		WHERE NAME = 'VLT_DB_POWERBI_NVCOMPLAINTTABLE'
 			AND TYPE = 'V'
 	)
@@ -18,21 +18,28 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
 CREATE VIEW [dbo].[VLT_DB_POWERBI_NVCOMPLAINTTABLE]
 AS
 	SELECT ComplaintTable.COMPLAINTID,
-	ComplaintTable.RESPONSIBLEUSERID AS 'Verantwortlicher',
-	ComplaintTable.SALESID,
-	ComplaintTable.SALESGROUP,
-	ComplaintTable.PRODUCTNAME,
-	ComplaintTable.APPROVED,
-	ComplaintTable.APPROVEDBY,
-	ComplaintTable.APPROVEDDATE,
-	ComplaintTable.COMPLETE,
-	ComplaintTable.COMPLAINTID	+ ComplaintTable.DATAAREAID AS PKComplaintID,
-	ComplaintTable.SalesID	+ ComplaintTable.DATAAREAID AS FKSalesID
+		   ComplaintTable.COMPLAINTCAUSEID,
+		   ComplaintTable.SALESID,
+		   ComplaintTable.DELIVERYMODEID,
+		   ComplaintTable.SALESGROUP,
+		   ComplaintTable.RESPONSIBLEUSERID AS 'Verantwortlicher',
+		   ComplaintTable.PRODUCTNAME,
+		   ComplaintTable.APPROVED,
+		   ComplaintTable.APPROVEDBY,
+		   ComplaintTable.APPROVEDDATE,
+		   ComplaintTable.COMPLETE,
+		   ComplaintTable.COMPLETEDATE,
+		   ComplaintTable.ADMINCOSTAMT,
+		   ComplaintTable.MATERIALCOSTAMT,
+		   ComplaintTable.SUBMONTEURCOSTAMT,
+		   ComplaintTable.MONTAGEID,
+		   ComplaintTable.COMPLAINTID + ComplaintTable.DATAAREAID AS PKComplaintID,
+		   ComplaintTable.SALESID + ComplaintTable.DATAAREAID AS FKSalesId,
+		   ComplaintTable.SALESGROUP + ComplaintTable.DATAAREAID AS FKSalesGroup,
+		   ComplaintTable.MONTAGEID + ComplaintTable.DATAAREAID AS FKMontageId
 	FROM NVCOMPLAINTTABLE AS ComplaintTable
-GO
 
+GO
