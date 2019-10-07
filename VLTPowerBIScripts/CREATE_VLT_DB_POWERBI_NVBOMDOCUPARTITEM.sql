74832d539589a2bbb25b820fd3e7967afec24501
Thomas Huemer <40458149+ThomasHuemer@users.noreply.github.com>
2019-10-07 16:13:03 +0200

    BomDocu Views

---
+++ VLTPowerBIScripts/CREATE_VLT_DB_POWERBI_NVBOMDOCUPARTITEM.sql
@@ -0,0 +1,61 @@
+USE [VLT_AX500105_P]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF EXISTS (
+		SELECT
+			1
+		FROM sys.views
+		WHERE NAME = 'VLT_DB_POWERBI_NVBOMDOCUPARTITEM'
+			AND TYPE = 'V'
+	)
+	DROP VIEW VLT_DB_POWERBI_NVBOMDOCUPARTITEM;
+GO
+
+CREATE VIEW [DBO].[VLT_DB_POWERBI_NVBOMDOCUPARTITEM]
+AS
+	SELECT
+		DocuPartItem.[PRODUCTID],
+		DocuPartItem.[POSITIONID],
+		DocuPartItem.[PARTID],
+		DocuPartItem.[ITEMID],
+		i.ITEMNAME,
+		DocuPartItem.[TXT],
+		DocuPartItem.[VERSIONID],
+		DocuPartItem.[COLORID],
+		DocuPartItem.[SIZEID],
+		DocuPartItem.[CHANGEREQ],
+		DocuPartItem.[CHANGETXT],
+		DocuPartItem.[BLOCKED],
+		DocuPartItem.[QTY],
+		DocuPartItem.[FIXEDSIZE],
+		DocuPartItem.[DATAAREAID],
+		DocuPartItem.[RECID],
+		DocuPartItem.[WIDTHADJ],
+		DocuPartItem.[HEIGHTADJ],
+		DocuPartItem.[CUTTOLENGTH],
+		DocuPartItem.[HEIGHTDEPENDENT],
+		DocuPartItem.[BENCHID],
+		DocuPartItem.[OPTIMIZE],
+		DocuPartItem.[OFFCUTADMIN],
+		DocuPartItem.[RECVERSION],
+		DocuPartItem.[VALUEX],
+		DocuPartItem.[VALUEY],
+		DocuPartItem.[VALUEZ],
+		DocuPartItem.[NOCUTLABELS],
+		DocuPartItem.[PICKSLIPUNITID],
+		DocuPartItem.[PICKSLIPSUMONLY],
+		DocuPartItem.[VLT_EXPLOREBOM],
+		DocuPartItem.PRODUCTID + DocuPartItem.DATAAREAID AS FKProductID,
+		DocuPartItem.VERSIONID + DocuPartItem.PRODUCTID + DocuPartItem.DATAAREAID AS FKVersionProductID,
+		DocuPartItem.POSITIONID + DocuPartItem.VERSIONID + DocuPartItem.PRODUCTID + DocuPartItem.DATAAREAID AS FKPositionVersionProductID,
+		DocuPartItem.PARTID + DocuPartItem.POSITIONID + DocuPartItem.VERSIONID + DocuPartItem.PRODUCTID + DocuPartItem.DATAAREAID AS FKPartPositionVersionProductID,
+		DocuPartItem.ITEMID + DocuPartItem.PARTID + DocuPartItem.POSITIONID + DocuPartItem.VERSIONID + DocuPartItem.PRODUCTID + DocuPartItem.DATAAREAID AS FKItemPartPositionVersionProductID
+	FROM [DBO].[NVBOMDOCUPARTITEM] AS DocuPartItem
+	LEFT JOIN INVENTTABLE i ON (i.ITEMID = DocuPartItem.ITEMID AND i.DATAAREAID = '100')
+GO
